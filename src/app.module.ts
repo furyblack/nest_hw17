3ff1fecdd30525ac96079e23b2533e4399c78957
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-06-12 15:29:52 +0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: '3991',
+      database: 'blogger_platform',
+      autoLoadEntities: false,
+      synchronize: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
