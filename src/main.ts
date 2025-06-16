ddd2412d973f820c5b9e821bad19577d3e13c49b
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-06-16 13:44:00 +0400

    add custom validator and use it global from main ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSetup } from './setup/app.setup';
-import { ValidationPipe } from '@nestjs/common';
+import { CustomValidationPipe } from './pipes/customValidationPipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true, // <-- Чтобы Nest знал как приводить типы без @Type в DTO
-      },
-    }),
-  );
+  app.useGlobalPipes(new CustomValidationPipe());
   appSetup(app);
   await app.listen(3000);
 }
